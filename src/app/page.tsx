df7123452aeee049b3f6c8214f2dc4ebc382e33b
Gerard Jardin <gerard@soltheory.com>
2026-01-03 16:59:24 +0000

    Move the Headline and Sub headline to the right a little bit without mov

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
                     <div className="flex-shrink-0">
                         <Logo className="w-40 h-40 md:w-52 md:h-52" />
                     </div>
-                    <div className="text-center md:text-left ml-16">
+                    <div className="text-center md:text-left ml-20">
                         <h1 className="font-headline text-6xl md:text-8xl font-bold tracking-tight">
                             SOL Theory
                         </h1>
