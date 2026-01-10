ede82d1dd132a16bf30a4c7cee01a8446dbfb49e
Gerard Jardin <gerard@soltheory.com>
2026-01-10 18:58:06 +0000

    its not showing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href={iconUrl} type="image/png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Questrial&display=swap" rel="stylesheet" />
