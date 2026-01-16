38f8d9757e0d3cb921ef69b62ef9218d5adbf967
Lucas Huff <lucas@soltheory.com>
2026-01-16 22:28:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5711990008-7ac2c",
+  "appId": "1:873103118314:web:a3329a68328d07aee56c93",
+  "apiKey": "AIzaSyCAJWBLJ1GTXtELpKFubBlENBq0eroUyCM",
+  "authDomain": "studio-5711990008-7ac2c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "873103118314"
+};
