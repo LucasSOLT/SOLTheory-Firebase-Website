075085ae8d74e3d231217dba539e2f9fddfe4a5f
Lucas Huff <lucas@soltheory.com>
2026-01-20 13:32:58 +0000

    Can you just make this webpage known as SOL Theory don't use the "SOL Th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { ProfileSetupDialog } from '@/components/auth/ProfileSetupDialog';
 const iconUrl = "https://firebasestorage.googleapis.com/v0/b/studio-5711990008-7ac2c.firebasestorage.app/o/SOL%20Theory%20Logo.png?alt=media&token=530d35ea-c595-4e88-bf37-6ec856485440";
 
 export const metadata: Metadata = {
-  title: 'SOL Theory Reimagined',
+  title: 'SOL Theory',
   description: 'Unlock Your Potential. Redefine Your Reality.',
   icons: {
     icon: iconUrl,
